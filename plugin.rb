7038ae923c44e9d1ea70f9d8848961084d244dde
Blake Walsh <blakeofthepath@gmail.com>
2015-05-14 20:25:56 +0300

    Made query deal better with deleted posts, and added tag information to results.

--- plugin.rb
+++ plugin.rb
@@ -21,11 +21,21 @@ after_initialize do
     requires_plugin 'suttacentral'
     def index
       raise Discourse::InvalidAccess unless guardian.is_admin?
-      time_query = "updated_at - ?::timestamp > interval '0'";
-      posts = Post.with_deleted.where(time_query, params[:since]).select('id, topic_id, post_number, cooked, hidden, user_deleted, updated_at, deleted_at')
-      topics = Topic.with_deleted.where(time_query, params[:since]).select('id, title, posts_count, updated_at, deleted_at, views, slug, category_id') 
-      categories = Category.where(time_query, params[:since]).select('id, name, slug, description, color, text_color, read_restricted, updated_at, parent_category_id')
-      
+      since = params[:since] or '1900-01-01T00:00:00.000Z'
+      time_query = "updated_at - :since::timestamp > interval '0.001 seconds' or deleted_at - :since::timestamp > interval '0.001 seconds'"
+      time_query_cat = "updated_at - :since::timestamp > interval '0.001 seconds'";
+      posts = Post.with_deleted
+                  .where(time_query, {since: params[:since]})
+                  .select('id, topic_id, post_number, cooked, hidden, user_deleted, updated_at, deleted_at')
+                  .as_json
+      topics = Topic.with_deleted
+                    .where(time_query, {since: params[:since]}).select('id, title, posts_count, updated_at, deleted_at, views, slug, category_id')
+                    .as_json
+      categories = Category.where(time_query_cat, {since: params[:since]})
+                           .select('id, name, slug, description, color, text_color, read_restricted, updated_at, parent_category_id')
+                           .as_json
+
+      topics.each {|t| t['tags'] = TopicCustomField.where(name: 'tags', topic_id: t['id']).order('updated_at').pluck(:value)}
       render json: {posts: posts, topics: topics, categories: categories}
     end
   end
